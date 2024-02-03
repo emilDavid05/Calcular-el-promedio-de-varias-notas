3cad92d9f31b84f2cd0ba654c03ffd23158f9683
Laptop <Laptop@DESKTOP-AAIMPLJ>
2024-02-03 18:32:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class ejercicio7
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la primera nota:");
+        double nota1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la segunda nota:");
+        double nota2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la tercera nota:");
+        double nota3 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la cuarta nota:");
+        double nota4 = Convert.ToDouble(Console.ReadLine());
+
+        double promedio = CalcularPromedio(nota1, nota2, nota3, nota4);
+
+        Console.WriteLine("El promedio de las 4 notas es: " + promedio);
+    }
+
+    static double CalcularPromedio(double nota1, double nota2, double nota3, double nota4)
+    {
+        return (nota1 + nota2 + nota3 + nota4) / 4;
+    }
+}
